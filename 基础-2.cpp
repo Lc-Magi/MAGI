001fe8a788507e8ad69920ce97e28d251daef0a8
Lc-Magi <2926763325@qq.com>
2019-01-20 14:09:57 +0800

    c

---
+++ 基础-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main (){
+	for (int i=0;i<=1;i++)
+	for (int j=0;j<=1;j++)
+    for (int k=0;k<=1;k++)
+	for (int r=0;r<=1;r++)
+	for (int p=0;p<=1;p++){ cout<<i<<j<<k<<r<<p<<endl;
+	}
+	return 0;
+}
