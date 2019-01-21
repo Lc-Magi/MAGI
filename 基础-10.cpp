d41bce86df8ae2e8908a0a39ab0216420d6792d2
Lc-Magi <2926763325@qq.com>
2019-01-21 17:18:19 +0800

    c

---
+++ 基础-10.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	long long int a;
+	cin>>dec>>a;
+	cout.setf(ios::uppercase);
+	cout<<hex<<a<<endl;
+	return 0;
+}
+	
